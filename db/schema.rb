326364c971e59297966dea2603054dd0c74d15ef
Riley Slayden <slaydenriley@gmail.com>
2020-02-24 03:02:04 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200224024858) do
+ActiveRecord::Schema.define(version: 20200224030019) do
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
