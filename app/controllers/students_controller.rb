1bd227150f797f1169050b6f40580bf209cc75fd
Riley Slayden <slaydenriley@gmail.com>
2020-02-24 02:59:37 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,6 @@ class StudentsController < ApplicationController
 
   def activate
     @student = Student.find(params[:id])
-    binding.pry
     @student.active = !@student.active
     @student.save
     redirect_to student_path(@student)
